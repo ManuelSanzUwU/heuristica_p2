403d141a3a7fa4ef4a8a9e521f89c17a83446a8c
OtpSyndraUwU <hexbugnano2003@gmail.com>
2023-12-10 22:49:00 +0100

    funciona el sh

--- CSP-calls.sh
+++ CSP-calls.sh
@@ -1,3 +1,3 @@
-python CSPParking.py ./CSP-test/parking01
-python CSPParking.py ./CSP-test/parking02
-python CSPParking.py ./CSP-test/parking03
+python3 CSPParking.py ./CSP-test/parking01.txt
+python3 CSPParking.py ./CSP-test/parking02.txt
+python3 CSPParking.py ./CSP-test/parking03.txt
