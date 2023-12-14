34fbc94ab001951c376173c30b08a2db5e456504
OtpSyndraUwU <hexbugnano2003@gmail.com>
2023-12-15 00:04:02 +0100

    primera heuristica

--- ASTARTraslados.py
+++ ASTARTraslados.py
@@ -15,7 +15,7 @@ class Estado:
 
     def __eq__(self, other):
         for i in range(len(self.mapa)):
-            for j in range(len(self.mapa)):
+            for j in range(len(self.mapa[i])):
                 if not other.mapa[i][j] == self.mapa[i][j]:
                     return False
 
@@ -23,27 +23,55 @@ class Estado:
             if not other.transporte[i] == self.transporte[i]:
                 return False
 
+        if len(other.transporte[2]) == len(self.transporte[2]):
+            for i in range(len(self.transporte[2])):
+                if not other.transporte[2].count("N") == self.transporte[2].count("N") \
+                        and not other.transporte[2].count("C") == self.transporte[2].count("C"):
+                    return False
+        else:
+            return False
+
         return self.energia == other.energia
+
+    def copiar_mapa(self) -> list:
+        m = []
+        for lista in self.mapa:
+            m.append(lista.copy())
+        return m
 
 
 class Nodo:
-    def __init__(self, valor, apuntando):
+    def __init__(self, valor: Estado, apuntando, c: int):
         self.valor = valor
         self.puntero = apuntando
+        self.c = c
+
+    def __str__(self):
+        return str(self.valor.mapa) + str(self.valor.transporte) + "e: " + str(self.valor.energia) + ", c:" + str(self.c)
 
     def __eq__(self, other):
-        self.valor.__eq__(other.valor)
+        return self.valor.__eq__(other.valor)
+
+    def impr_sol(self):
+        if self.puntero is None:
+            return self.__str__()
+        else:
+            return self.puntero.impr_sol() + "\n" + self.__str__()
 
     def expandir(self) -> list:
-        z = [Nodo(self.mover_a((self.valor.transporte[0]+1, self.valor.transporte[1])), None),
-             Nodo(self.mover_a((self.valor.transporte[0]-1, self.valor.transporte[1])), None),
-             Nodo(self.mover_a((self.valor.transporte[0], self.valor.transporte[1]+1)), None),
-             Nodo(self.mover_a((self.valor.transporte[0], self.valor.transporte[1]-1)), None)]
+        z = [Nodo(self.mover_a((self.valor.transporte[0]+1, self.valor.transporte[1],
+                                self.valor.transporte[2].copy())), None, self.c + 1),
+             Nodo(self.mover_a((self.valor.transporte[0]-1, self.valor.transporte[1],
+                                self.valor.transporte[2].copy())), None, self.c + 1),
+             Nodo(self.mover_a((self.valor.transporte[0], self.valor.transporte[1]+1,
+                                self.valor.transporte[2].copy())), None, self.c + 1),
+             Nodo(self.mover_a((self.valor.transporte[0], self.valor.transporte[1]-1,
+                                self.valor.transporte[2].copy())), None, self.c + 1)]
         j = 0
         for i in range(len(z)):
             if z[i-j].valor is None:
-                j+=1
                 z.__delitem__(i-j)
+                j += 1
         return z
 
     def coste(self) -> int:
@@ -56,13 +84,43 @@ class Nodo:
         self.puntero = obj
 
     def mover_a(self, tr):
+        if tr[0] < 0 or tr[1] < 0:
+            return None
         if len(self.valor.mapa) <= tr[0] or len(self.valor.mapa[tr[0]]) <= tr[1]:
             return None
         z = self.valor.mapa[tr[0]][tr[1]]
+        mapa = self.valor.copiar_mapa()
         e = self.valor.energia
         if z.isdigit():
             e -= int(z)
-        return Estado(self.valor.mapa, tr, e)
+        else:
+            if z == "N":
+                if len(tr[2]) < 10 and "C" not in tr[2]:
+                    tr[2].append("N")
+                    mapa[tr[0]][tr[1]] = "1"
+                e -= 1
+            elif z == "C":
+                if len(tr[2]) < 10 and tr[2].count("N") <= 8:
+                    tr[2].append("C")
+                    mapa[tr[0]][tr[1]] = "1"
+                e -= 1
+            elif z == "CN":
+                if "C" not in tr[2]:
+                    tr[2].clear()
+                e -= 1
+            elif z == "CC":
+                for i in range(tr[2].count("C")):
+                    tr[2].pop(tr[2].index("C"))
+                e -= 1
+            elif z == "X":
+                return None
+            elif z == "P":
+                e = 50
+        if e <= 0:
+            return None
+
+        r = Estado(mapa, tr, e)
+        return r
 
 
 class Grafo:
@@ -72,12 +130,27 @@ class Grafo:
     def añadir(self, nodo):
         self.nodos.append(nodo)
 
-    def contiene(self, estado) -> bool:
-        """recibe un estado y devuleve true si el estado ya esta presente en el grafo"""
+
+def f(nodo: Nodo):
+    return heuristica_1(nodo) + nodo.c
 
 
-def heuristica_1(estado):
-    return 4
+def heuristica_sin_informar(nodo: Nodo):
+    return 50
+
+
+def heuristica_1(nodo: Nodo):
+    estado = nodo.valor
+    n = 0
+    c = 0
+    for i in range(len(estado.mapa)):
+        for j in range(len(estado.mapa[i])):
+            if estado.mapa[i][j] == "N":
+                n += 1
+            elif estado.mapa[i][j] == "C":
+                c += 1
+    return (n + c)*2 + len(estado.transporte[2])
+
 
 
 def generar_e_final(e_inicial: list) -> list:
@@ -97,56 +170,69 @@ def localizar_parking(mapa: list) -> tuple:
     for i in range(len(mapa)):
         for j in range(len(mapa[i])):
             if mapa[i][j] == "P":
-                return i, j
+                return i, j, []
+
+
+def esta(nodo, lista) -> bool:
+    for e in lista:
+        if e.__eq__(nodo):
+            return True
+    return False
 
 
 def a_estella(estado_inicial: Estado, estado_final: Estado):
-    I = Nodo(estado_inicial, None)
+    I = Nodo(estado_inicial, None, 0)
     G = Grafo([I])
     abierta = [I]
     cerrada = []
     falso = False
-    while len(abierta) or falso:
+    iteracion = 0
+    sol = None
+    while len(abierta) and not falso:
         n = abierta[0]
+        appends = []
+        print("se expande el ndo:", n)
         abierta.__delitem__(0)
-        cerrada.append(n)
         if estado_final.__eq__(n.valor):
             falso = True
+            sol = n
         else:
             S = n.expandir()
+            cerrada.append(n)
             for s in S:
-                if not G.contiene(s):
+                if not(esta(s, abierta) or esta(s, cerrada)):
                     G.añadir(s.apuntar(n))
-                esta = False
-                for e in abierta:
-                    if e.__eq__(s):
-                        if s.coste() < e.coste():
-                            e.apuntar(n)
-                            esta = True
-                if not esta:
-                    abierta.append(s)
-
-                esta = False
-                for e in cerrada:
-                    if e.__eq__(s):
-                        if s.coste() < e.coste():
-                            e.apuntar(n)
-                            esta = True
-                if not esta:
-                    cerrada.append(s)
+                    appends.append(s)
+                elif esta(s, abierta):
+                    for i in range(len(abierta)):
+                        if abierta[i].__eq__(s):
+                            if f(s) < f(abierta[i]):
+                                abierta[i] = s
+                                break
 
             lista = []
+            abierta += appends
             for i in range(len(abierta)):
-                min = heuristica_1(abierta[0])
+                min = f(abierta[0])
                 z = 0
                 for j in range(len(abierta)):
-                    if heuristica_1(abierta[j]) < min:
+                    if f(abierta[j]) < min:
                         z = j
                 lista.append(abierta[z])
+                abierta.__delitem__(z)
             abierta = lista
-        print(abierta)
 
-    print(falso)
+        print(iteracion, falso, abierta, cerrada)
+        for nodo in abierta:
+            ...
+            # print(nodo.valor.mapa, nodo.valor.transporte, nodo.valor.energia, "f=", heuristica_sin_informar(nodo))
+
+        iteracion += 1
+
+        print()
+
+    if falso:
+        print(sol.impr_sol())
 
 
 inicial = lector.leer(sys.argv[1])
@@ -156,4 +242,11 @@ I = Estado(inicial, localizar_parking(inicial), 50)
 F = Estado(final, localizar_parking(inicial), 50)
 
 a_estella(I, F)
+
+"""
+a = Nodo(Estado(inicial, (1, 1, ["N", "C", "C"]), 50), None, 0)
+b = Nodo(Estado(inicial, (1, 1, ["C", "N", "C"]), 50), a, 2)
+print(a.__str__() + "\n" + b.__str__())
+print(a.__eq__(b))
+"""
 
